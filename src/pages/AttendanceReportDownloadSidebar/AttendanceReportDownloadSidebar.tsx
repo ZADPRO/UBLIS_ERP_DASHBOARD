4d7dfdc6ecfd6a9400b4c5207aa65eff15f82652
IONHAX04 <thirukumara04@gmail.com>
2024-12-27 20:36:09 +0530

    attendance report - drafted

--- src/pages/AttendanceReportDownloadSidebar/AttendanceReportDownloadSidebar.tsx
+++ src/pages/AttendanceReportDownloadSidebar/AttendanceReportDownloadSidebar.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
 import { Nullable } from "primereact/ts-helpers";
 import { Calendar } from "primereact/calendar";
@@ -6,6 +12,10 @@ import axios from "axios";
 import CryptoJS from "crypto-js";
 import { MultiSelect } from "primereact/multiselect";
 import { Button } from "primereact/button";
+import { Toast } from "primereact/toast";
+import { Toolbar } from "primereact/toolbar";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
 
 interface SessionType {
   name: string;
@@ -37,7 +47,37 @@ interface GroupedOption {
 
 type DecryptResult = any;
 
+interface Product {
+  id: string | null;
+  code: string;
+  name: string;
+  description: string;
+  image: string | null;
+  price: number;
+  category: string | null;
+  quantity: number;
+  inventoryStatus: string;
+  rating: number;
+}
+
+interface Attendance {
+  emp_code: string;
+  punch_date: string;
+  punch_time: string;
+  refStFName: string;
+  refStLName: string;
+  interval_start: string;
+  interval_end: string;
+}
+
+interface FlattenedData extends Attendance {
+  packageName: string;
+  sessionMode: string;
+}
+
 const AttendanceReportDownloadSidebar: React.FC = () => {
+  const toast = useRef<Toast>(null);
+  const dt = useRef<DataTable<Product[]>>(null);
   const [sessionMode, setSessionMode] = useState<SessionType[]>([]);
   const [reportRange, setReportRange] = useState<ReportRangeParams | null>(
     null
@@ -53,6 +93,7 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
   const [selectedDropdownValue, setSelectedDropdownValue] = useState<string[]>(
     []
   );
+  const [attendanceData, setAttendanceData] = useState<FlattenedData[]>([]);
 
   const session: SessionType[] = [
     { name: "Online", code: 1 },
@@ -118,6 +159,15 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
     }
   };
 
+  const formatMonthYear = (date: Nullable<Date>): string => {
+    return date
+      ? new Intl.DateTimeFormat("en-GB", {
+          month: "2-digit",
+          year: "numeric",
+        }).format(date)
+      : "";
+  };
+
   useEffect(() => {
     if (reportRange?.code === 1 && date) {
       const selectedDate = new Intl.DateTimeFormat("en-GB", {
@@ -134,7 +184,7 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
     } else if (reportRange?.code === 2) {
       handleApiCall("");
     }
-  }, [reportRange, date, sessionMode]);
+  }, [reportRange, date, sessionMode, toMonth]);
 
   const groupedData: GroupedOption[] = useMemo(() => {
     if (!attendanceOptions) {
@@ -142,6 +192,8 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
     }
 
     return attendanceOptions.reduce((acc, curr) => {
+      console.log("curr", curr);
+      console.log("acc", acc);
       const existingGroup = acc.find(
         (group) => group.label === curr.refPackageName
       );
@@ -186,6 +238,92 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
     setDate(e.value);
   }, []);
 
+  const reportAPI = async () => {
+    try {
+      const refRepDurationFormatted = date
+        ? new Intl.DateTimeFormat("en-GB", {
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true,
+          }).format(date)
+        : "";
+
+      const refRepDuType = reportRange?.code;
+
+      const refSessionMod = sessionMode.map((session) => session.code);
+
+      const res = await axios.post(
+        import.meta.env.VITE_API_URL + "/attendance/report",
+        {
+          refRepDuration: refRepDurationFormatted,
+          refRepDuType: refRepDuType,
+          refPackageId: selectedDropdownValue,
+          refSessionMod: refSessionMod,
+        },
+        {
+          headers: {
+            Authorization: localStorage.getItem("JWTtoken"),
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      const data = decrypt(
+        res.data[1],
+        res.data[0],
+        import.meta.env.VITE_ENCRYPTION_KEY
+      );
+
+      console.log("Decrypted Data:", data);
+      const flattened: FlattenedData[] = data.reportData.flatMap((report) =>
+        (report.attendance || []).map((att) => ({
+          ...att,
+          packageName: report.packageName,
+          sessionMode: report.sessionMode,
+        }))
+      );
+      console.log("flattened", flattened);
+
+      setAttendanceData(flattened);
+    } catch (error) {
+      console.error("Error calling API:", error);
+    }
+  };
+
+  const exportCSV = () => {
+    dt.current?.exportCSV();
+  };
+
+  const rightToolbarTemplate = () => {
+    return (
+      <Button label="Export" className="p-button-help" onClick={exportCSV} />
+    );
+  };
+
+  const headerTemplate = (data: FlattenedData) => {
+    return (
+      <div className="flex flex-column">
+        <div className="font-bold">{data.packageName}</div>
+        <small>Session Mode: {data.sessionMode}</small>
+      </div>
+    );
+  };
+
+  const footerTemplate = (data: FlattenedData) => {
+    return (
+      <div className="font-bold">
+        Total Records:{" "}
+        {
+          attendanceData.filter((item) => item.packageName === data.packageName)
+            .length
+        }
+      </div>
+    );
+  };
   return (
     <div className="m-2">
       <label htmlFor="calendar-12h" className="font-bold block mb-2">
@@ -263,9 +401,76 @@ const AttendanceReportDownloadSidebar: React.FC = () => {
             display="chip"
             className="w-full md:w-20rem mt-3"
           />
-          <Button label="Success" className="mt-3" severity="success" />
+          <Button
+            onClick={reportAPI}
+            label="Submit"
+            className="mt-3"
+            severity="success"
+          />
         </div>
       )}
+
+      {reportRange &&
+        date &&
+        groupedDropdownOptions &&
+        selectedDropdownValue && (
+          <>
+            <Toast ref={toast} />
+            <div className="card mt-4">
+              <Toolbar className="mb-4" right={rightToolbarTemplate}></Toolbar>
+
+              <DataTable
+                value={attendanceData}
+                rowGroupMode="subheader"
+                groupRowsBy="packageName"
+                sortMode="single"
+                ref={dt}
+                sortField="packageName"
+                sortOrder={1}
+                rowGroupHeaderTemplate={headerTemplate}
+                rowGroupFooterTemplate={footerTemplate}
+                scrollable
+                scrollHeight="400px"
+                tableStyle={{ minWidth: "50rem" }}
+              >
+                <Column
+                  field="emp_code"
+                  frozen
+                  header="Employee Code"
+                  style={{ minWidth: "150px" }}
+                ></Column>
+                <Column
+                  field="refStFName"
+                  header="Employee Code"
+                  body={(rowData) =>
+                    `${rowData.refStFName} ${rowData.refStLName}`
+                  }
+                  style={{ minWidth: "150px" }}
+                ></Column>
+                <Column
+                  field="punch_date"
+                  header="Punch Date"
+                  style={{ minWidth: "150px" }}
+                ></Column>
+                <Column
+                  field="punch_time"
+                  header="Punch Time"
+                  style={{ minWidth: "150px" }}
+                ></Column>
+                <Column
+                  field="interval_start"
+                  header="Interval Start"
+                  style={{ minWidth: "150px" }}
+                ></Column>
+                <Column
+                  field="interval_end"
+                  header="Interval End"
+                  style={{ minWidth: "150px" }}
+                ></Column>
+              </DataTable>
+            </div>
+          </>
+        )}
     </div>
   );
 };
